6fb19f983c04498cd3f90eebb08d957f1b327877
LaraKraemer <51157020+LaraKraemer@users.noreply.github.com>
2024-07-08 14:01:32 +0200

    Modify django admin interface

--- job_application/admin.py
+++ job_application/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 from .models import Form
 
 
-admin.site.register(Form)
+# class ModelAdmin designed to modify admin interfaces
+class FormAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "email", "date", "occupation")
+    search_fields = ("first_name", "last_name", "email", "date", "occupation")
+    list_filter = ("date", "occupation")
+    odering = ("first_name",)
+    readonly_fields = ("occupation", )
+
+admin.site.register(Form, FormAdmin)
